c3c0eb09be10895496d17b446c49da5c810dfa8c
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-03-27 23:52:36 -0300

    finish ui

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -1,11 +1,50 @@
 import { CartComponent } from "./styles";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+const data = [
+    {
+        id: 1,
+        img: "http://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        img2: "http://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        title: "Long Sleeve Graphic T-shirt",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, magnam a? Recusandae, impedit debitis sequi sint incidunt laborum dolorem blanditiis a hic possimus reprehenderit! Voluptates excepturi quae omnis ab porro!",
+        isNew: true,
+        oldPrice: 19,
+        price: 12,
+    },
+    {
+        id: 1,
+        img: "http://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        img2: "http://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        title: "Long Sleeve Graphic T-shirt",
+        desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, magnam a? Recusandae, impedit debitis sequi sint incidunt laborum dolorem blanditiis a hic possimus reprehenderit! Voluptates excepturi quae omnis ab porro!",
+        isNew: true,
+        oldPrice: 19,
+        price: 12,
+    },
+];
 
 export function Cart() {
-    
-
-    return(
+    return (
         <CartComponent>
-            <h2>Cart</h2>
+            <h1>Products in your Cart</h1>
+            {data?.map((item) => (
+                <div className="item" key={item.id}>
+                    <img src={item.img} alt="" />
+                    <div className="details">
+                        <h1>{item.title}</h1>
+                        <p>{item.desc?.substring(0, 50)}</p>
+                        <div className="price">1x ${item.price}</div>
+                        <DeleteOutlinedIcon className="delete" />
+                    </div>
+                </div>
+            ))}
+            <div className="total">
+                <span>SUBTOTAL</span>
+                <span>$ 123</span>
+            </div>
+            <button>PROCEED TO PAYMENT</button>
+            <span className="reset">Reset Cart</span>
         </CartComponent>
-    )
+    );
 }
